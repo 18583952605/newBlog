63790d3e637468263800d05deeff7301aee1293d
wzy <657189555@qq.com>
2021-07-25 00:21:43 +0800

    code: history改为hash模式

--- .umirc.ts
+++ .umirc.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'dumi'
 export default defineConfig({
   title: 'newNote',
   mode: 'site',
+  history: { type: 'hash' },
   locales: [['zh-CN', '中文']],
   // plugins: ['./plugins/handle.ts'],
   publicPath: process.env.NODE_ENV === 'development' ? undefined : '/newBlog/',
